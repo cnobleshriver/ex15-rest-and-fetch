50af6feffd89db2b726d0f8d6783dcdeec56e37e
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-04-11 14:09:19 -0400

    first commit

---
+++ src/starbuddy.js
@@ -0,0 +1,13 @@
+// TASK #3: Fetch the Stars
+async function getStars() {
+  // Replace the line below with your implementation.
+  throw new Error("Not implemented");
+}
+
+// TASK #2: Add Event Listener
+function initialize() {
+  // Replace the line below with your implementation.
+  throw new Error("Not implemented");
+}
+
+initialize();
