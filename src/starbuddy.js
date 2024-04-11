983a9bd3fa2f00f8910d90ab54a4c88ff5b00d78
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-04-11 14:40:03 -0400

    fix task #3

--- src/starbuddy.js
+++ src/starbuddy.js
@@ -1,12 +1,13 @@
 // TASK #3: Fetch the Stars
 async function getStars() {
   let url = "https://api.github.com/repos/" + document.getElementById("repo").value;
+  console.log(url);
 
   try {
     let response = await fetch(url);
     if (response.ok) {
       let json = await response.json();
-      document.getElementById("star_count").innerHTML = json.stargazers.count;
+      document.getElementById("star_count").innerHTML = json["stargazers_count"];
     } else {
       document.getElementById("star_count").innerHTML = "An error has occurred";
     }
